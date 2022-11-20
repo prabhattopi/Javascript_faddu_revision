39914e3770fa221c867c1ad03444a7dc5b1f04e9
prabhattopi <topi9864@gmail.com>
2022-11-20 16:11:55 +0530

    The file is with Ben base expo shorhand e==10

---
+++ Ben.js
@@ -0,0 +1,11 @@
+//beN;=====>b-->base number and e char (which is seperator or exponential) and N-->Power
+
+
+function newFunction(){
+    // return 5e1;//50 kaise (5 * aur kitna zeor laga hai matlab 10 ki power)
+    // return 1e3;
+    // return 1e-3;//(1*10**-3)
+     return 0e+5;//bas e matlab 10 bas itna yadd rakho
+}
+
+console.log(newFunction())
