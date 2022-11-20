a01eb1d5d902386917f1f795d06ff35eeeae6368
prabhattopi <topi9864@gmail.com>
2022-11-20 16:39:30 +0530

    The file is getting update with forof forin index

---
+++ ForinvsForof.js
@@ -0,0 +1,13 @@
+//Diff for of for in
+
+
+const names=["prabhat","Singh","hello","world"]
+
+for (const i of names) {
+    
+    console.log(i)//element dikhega 
+}
+for (const i in names) {
+    
+    console.log(i)//index no dikhega (in-dex)
+}
